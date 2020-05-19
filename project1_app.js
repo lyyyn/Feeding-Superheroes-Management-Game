d6c3f86c28277b73aaa8368a5e8793cdbea5f12d
Linawaty <linawatylim@gmail.com>
2020-05-19 18:50:10 +0800

    Create shop class to track global var, steamline uniquehero generation, serve food completed

--- project1_app.js
+++ project1_app.js
@@ -3,37 +3,41 @@ const getRandom = (arr) => {
     return arr[Math.floor(Math.random() * arr.length)];
 }
 
-const generateRandomSuperheroID = () => {
-    return Math.ceil(Math.random() * NUM_OF_SUPERHEROES);
-}
-
-const generateRandomFoodItem = (percentage, arr) => { //probability of returning null or any of the array
+const generateRandomFoodItem = (percentage, arrPossibleFood) => { //probability of returning null or any of the array
     if ((Math.floor(Math.random() * 101)) <= percentage) {
         return null;
     } else {
-        return todayFood[getRandom(arr)];
+        return shop.todayFood[getRandom(arrPossibleFood)];
     };
 }
 
-const orderFormat = (arr) => {
-    let saying = '';
-    saying += getRandom(ORDER_GREETINGS) + '</br>';
-    arr.forEach((element, index) => {
-        if (element != null) { saying += element.foodName + '</br>' };
-    });
-    return saying;
+//classes
+class Shop {
+    constructor() {
+        this.todayCust = [];
+        this.todayFood = [];
+        this.todayUniqueCustID = [];
+    }
+    getCustomerByID(id) {
+        return this.todayCust.find(customer => customer.custID === id);
+    }
+    getFoodByID(id) {
+        return this.todayFood.find(food => food.foodID === id);
+    }
 }
 
-//classes
 class Customer {
     constructor(cID, cName, cURL) {
-        this.custID = cID;
+        this.custID = cID; //408  xxx.find(408).custName
         this.custName = cName;
         this.custMoney = 100;
         this.custImage = cURL;
         this.custPatience = 5; //willing to wait for 5 secs
         this.custOrder = [];
+        this.custFulfilled = [];
         this.custSlot = 0;
+        this.custOrderSaying = '';
+        this.custOrderCompleted = true
     }
     enter() {
         //enter the store
@@ -42,24 +46,63 @@ class Customer {
         this.custSlot = $('.empty:eq(0)').attr('id');
         $('.empty:eq(0)').removeClass('empty');
         $('#' + this.custSlot)
-            .css('background-image', `url("${this.custImage}")`);
+            .css('background-image', `url("${this.custImage}")`)
+            .attr('custid', this.custID);
     }
     greet() {
         //say something
         $('#' + this.custSlot)
             .html(`<span>${getRandom(ENTRY_GREETINGS)}</span>`);
     }
-    order(cOrder) {
-        this.custOrder = cOrder;
+    order(arr4OrderedFoods) {
+        this.custOrder = arr4OrderedFoods;
+        this.sayOrder();
+    }
+    sayOrder() {
+        let saying = '';
+        saying += getRandom(ORDER_GREETINGS) + '</br>';
+        this.custOrder.forEach((orderedFood, index) => {
+            if (this.custFulfilled.indexOf(orderedFood) >= 0) {
+                saying += `<span class="strikethrough">${orderedFood.foodName}</span></br>`;
+            } else if (orderedFood != null) {
+                saying += `${orderedFood.foodName} </br>`;
+            };
+        });
+
+        this.custOrderSaying = saying;
+
         //display the order bubble
         $('#' + this.custSlot)
-            .html(`<span>${orderFormat(cOrder)}</span>`);
+            .html(`<span>${this.custOrderSaying}</span>`);
+    }
+    checkOrder(servedFood) {
+        console.log('servedFood: ', servedFood);
+        console.log('custOrder: ', this.custOrder);
+        if (this.custOrder.indexOf(servedFood) >= 0) {
+            this.custFulfilled.push(servedFood);
+            console.log('custFulfilled: ', this.custFulfilled);
+            this.sayOrder();
+            if (this.isOrderCompleted()) {
+                this.leave();
+            };
+        } else {
+            console.log('complain, served wrong food');
+        }
+    }
+    isOrderCompleted() {
+        if (this.custOrder.length === this.custFulfilled.length) {
+            return true;
+        } else {
+            return false;
+        };
     }
     eat() {
 
     }
     leave() {
-        //enter the store
+        //leave the store
+        console.log(this.custName + ' leave store');
+
     }
 
 
@@ -106,6 +149,7 @@ class DailySummary {
 }
 
 //instances
+const shop = new Shop([]);
 const singapore = new FoodOrigin('Singapore', 'SGD', 1);
 const china = new FoodOrigin('China', 'CNY', 4.5);
 const thailand = new FoodOrigin('Thailand', 'THB', 20.808);
@@ -128,20 +172,17 @@ const ENTRY_GREETINGS = ['Hullo', 'Hi', 'Ooosh!', 'How do you do', 'Goodday', 'G
 const ORDER_GREETINGS = ['I want to order ', 'Gimme ', 'Maybe I\'ll get ', 'For today, it\'ll be ', 'Please give me ', 'How about ', 'I\'d like ', 'Just '];
 const DAY_SUMMARY = ['', 'Summary of Day 1', 'Summary of Day 2', 'Summary of Day 3', 'Summary of Day 4', 'Summary of Day 5'];
 const FOOD_ARR = [ //plan to read from files in the future
-    {foodID:0,foodName:'Oolong Tea',foodIngredients:'Tea',foodOrigin:'China',foodType:'Drink',foodMethod:'Boil',foodCostFrSupplier:6,foodPriceinSGD:2,foodRating:2,foodQty:10,foodImage:'https://i.ibb.co/jDVRwQ0/drink-tea.png'},
-    {foodID:1,foodName:'Arabica Coffee',foodIngredients:'Coffee',foodOrigin:'Indonesia',foodType:'Drink',foodMethod:'Boil',foodCostFrSupplier:15000,foodPriceinSGD:2.5,foodRating:2,foodQty:10,foodImage:'https://i.ibb.co/5xy7jMz/drink-coffee.png'},
-    {foodID:2,foodName:'Kunlun Water',foodIngredients:'Water',foodOrigin:'China',foodType:'Drink',foodMethod:'Boil',foodCostFrSupplier:5,foodPriceinSGD:1.5,foodRating:1,foodQty:10,foodImage:'https://i.ibb.co/kQ9GXxS/drink-water.png'},
-    {foodID:3,foodName:'Yunnan Broccoli',foodIngredients:'Broccoli',foodOrigin:'China',foodType:'Veggie',foodMethod:'Steam',foodCostFrSupplier:9,foodPriceinSGD:4,foodRating:1,foodQty:10,foodImage:'https://i.ibb.co/7b33T88/food-steam-Broccoli.png'},
-    {foodID:4,foodName:'Egg Custard',foodIngredients:'Egg',foodOrigin:'China',foodType:'Protein',foodMethod:'Steam',foodCostFrSupplier:15.6,foodPriceinSGD:5.25,foodRating:1,foodQty:10,foodImage:'https://i.ibb.co/GcPm5wf/food-steam-Egg.png'},
-    {foodID:5,foodName:'Steam Tofu',foodIngredients:'Tofu',foodOrigin:'China',foodType:'Protein',foodMethod:'Steam',foodCostFrSupplier:12.5,foodPriceinSGD:4.5,foodRating:1,foodQty:10,foodImage:'https://i.ibb.co/yy2GcX8/food-steam-Tofu.png'},
-    {foodID:6,foodName:'Hainan Chicken',foodIngredients:'Chicken',foodOrigin:'China',foodType:'Meat',foodMethod:'Steam',foodCostFrSupplier:18.25,foodPriceinSGD:6,foodRating:1,foodQty:10,foodImage:'https://i.ibb.co/whGnH4k/food-steamed-Chicken.png'},
-    {foodID:7,foodName:'Steam Fish',foodIngredients:'Fish',foodOrigin:'China',foodType:'Meat',foodMethod:'Steam',foodCostFrSupplier:20.3,foodPriceinSGD:7,foodRating:2,foodQty:10,foodImage:'https://i.ibb.co/rssrvZL/food-steam-Fish.png'},
-    {foodID:8,foodName:'Thai Rice',foodIngredients:'Rice',foodOrigin:'Thailand',foodType:'Carbo',foodMethod:'Steam',foodCostFrSupplier:40,foodPriceinSGD:3,foodRating:1,foodQty:10,foodImage:'https://i.ibb.co/TvddwdV/food-Rice.png'}
+    { foodID: 0, foodName: 'Oolong Tea', foodIngredients: 'Tea', foodOrigin: 'China', foodType: 'Drink', foodMethod: 'Boil', foodCostFrSupplier: 6, foodPriceinSGD: 2, foodRating: 2, foodQty: 10, foodImage: 'https://i.ibb.co/jDVRwQ0/drink-tea.png' },
+    { foodID: 1, foodName: 'Arabica Coffee', foodIngredients: 'Coffee', foodOrigin: 'Indonesia', foodType: 'Drink', foodMethod: 'Boil', foodCostFrSupplier: 15000, foodPriceinSGD: 2.5, foodRating: 2, foodQty: 10, foodImage: 'https://i.ibb.co/5xy7jMz/drink-coffee.png' },
+    { foodID: 2, foodName: 'Kunlun Water', foodIngredients: 'Water', foodOrigin: 'China', foodType: 'Drink', foodMethod: 'Boil', foodCostFrSupplier: 5, foodPriceinSGD: 1.5, foodRating: 1, foodQty: 10, foodImage: 'https://i.ibb.co/kQ9GXxS/drink-water.png' },
+    { foodID: 3, foodName: 'Yunnan Broccoli', foodIngredients: 'Broccoli', foodOrigin: 'China', foodType: 'Veggie', foodMethod: 'Steam', foodCostFrSupplier: 9, foodPriceinSGD: 4, foodRating: 1, foodQty: 10, foodImage: 'https://i.ibb.co/7b33T88/food-steam-Broccoli.png' },
+    { foodID: 4, foodName: 'Egg Custard', foodIngredients: 'Egg', foodOrigin: 'China', foodType: 'Protein', foodMethod: 'Steam', foodCostFrSupplier: 15.6, foodPriceinSGD: 5.25, foodRating: 1, foodQty: 10, foodImage: 'https://i.ibb.co/GcPm5wf/food-steam-Egg.png' },
+    { foodID: 5, foodName: 'Steam Tofu', foodIngredients: 'Tofu', foodOrigin: 'China', foodType: 'Protein', foodMethod: 'Steam', foodCostFrSupplier: 12.5, foodPriceinSGD: 4.5, foodRating: 1, foodQty: 10, foodImage: 'https://i.ibb.co/yy2GcX8/food-steam-Tofu.png' },
+    { foodID: 6, foodName: 'Hainan Chicken', foodIngredients: 'Chicken', foodOrigin: 'China', foodType: 'Meat', foodMethod: 'Steam', foodCostFrSupplier: 18.25, foodPriceinSGD: 6, foodRating: 1, foodQty: 10, foodImage: 'https://i.ibb.co/whGnH4k/food-steamed-Chicken.png' },
+    { foodID: 7, foodName: 'Steam Fish', foodIngredients: 'Fish', foodOrigin: 'China', foodType: 'Meat', foodMethod: 'Steam', foodCostFrSupplier: 20.3, foodPriceinSGD: 7, foodRating: 2, foodQty: 10, foodImage: 'https://i.ibb.co/rssrvZL/food-steam-Fish.png' },
+    { foodID: 8, foodName: 'Thai Rice', foodIngredients: 'Rice', foodOrigin: 'Thailand', foodType: 'Carbo', foodMethod: 'Steam', foodCostFrSupplier: 40, foodPriceinSGD: 3, foodRating: 1, foodQty: 10, foodImage: 'https://i.ibb.co/TvddwdV/food-Rice.png' }
 ]
 
-const todayFood = [];
-const todayUniqueCustID = [];
-const todayCust = [];
 
 
 let currDay = ZERO;
@@ -164,8 +205,13 @@ const randomizeCustOrder = () => { //array of 4 food item from todayFood
     //3rd random between null or 4-7 (protein/meat) -> null = not ordering protein/meat
     //4th random between null or 8 (rice) -> null = not ordering rice
     ORDER_COMPOSITION.forEach(element => {
-        cOrder.push(generateRandomFoodItem(ORDER_NULL_PROBABILITY, element));
+        let randomFoodItem = generateRandomFoodItem(ORDER_NULL_PROBABILITY, element);
+        if (randomFoodItem !== null) {
+            cOrder.push(randomFoodItem);
+        }
     });
+    //to ensure cust min order 1 item
+    if (cOrder.length === 0) { cOrder.push(generateRandomFoodItem(ZERO, element)) };
     return cOrder;
 };
 
@@ -173,51 +219,39 @@ const buildTodayFOOD_ARR = (num) => {
     let food = null;
     FOOD_ARR.forEach(obj => {
         food = new Food(obj);
-        todayFood.push(food);
+        shop.todayFood.push(food);
         // console.log(food);
     });
-    // console.log(todayFood); /* debug */
+    // console.log(shop.todayFood); /* debug */
+}
+
+const checkOrder = (foodID, custID) => {
+    const thisCust = shop.getCustomerByID(custID);
+    thisCust.checkOrder(shop.getFoodByID(parseInt(foodID)));
 }
 
 const displayFood = () => {
-    todayFood.forEach((element, index) => {
+    shop.todayFood.forEach((element, index) => {
         //place in the tray
         $('.food:nth-child(' + (index + 1) + ')')
-            .html(`<img src="${element.foodImage}" class="draggable" id="${element.foodID}">`)
+            .html(`<img src="${element.foodImage}" class="draggable" id="${element.foodID}" title="${element.foodName}">`)
     });
     //make food draggable
     $(".draggable").draggable({ helper: "clone" });
-    //make box droppable
-    $(".box").droppable({
+    //make customer droppable
+    $(".customer").droppable({
         accept: ".draggable",
         drop: function (event, ui) {
-            console.log("drop");
-            $(this).removeClass("border").removeClass("over").removeClass("ui-droppable");
-            let dropped = ui.draggable;
-            let droppedOn = $(this);
-            $(dropped).detach().css({ top: 0, left: 0 }).appendTo(droppedOn);
+            $(this).removeClass("border").removeClass("over");
+            checkOrder(ui.draggable.attr('id'), $(event.target).attr('custid'));
         },
         over: function (event, elem) {
             $(this).addClass("over");
-            console.log("over");
-        }
-        ,
+        },
         out: function (event, elem) {
             $(this).removeClass("over");
         }
     });
-
-    // $("#origin").droppable({
-    //     accept: ".draggable", drop: function (event, ui) {
-    //         console.log("drop");
-    //         $(this).removeClass("border").removeClass("over");
-    //         var dropped = ui.draggable;
-    //         var droppedOn = $(this);
-    //         $(dropped).detach().css({ top: 0, left: 0 }).appendTo(droppedOn);
-
-
-    //     }
-    // });
 }
 
 const prepareFood = () => {
@@ -230,9 +264,9 @@ const resetVars = () => {
     earning = ZERO;
     happyCust = ZERO;
     currCost = [];
-    todayFood.splice(ZERO, todayFood.length); //empty array without reassigning
-    todayCust.splice(ZERO, todayCust.length);
-    todayUniqueCustID.splice(ZERO, todayUniqueCustID.length);
+    shop.todayFood.splice(ZERO, shop.todayFood.length); //empty array without reassigning
+    shop.todayCust.splice(ZERO, shop.todayCust.length);
+    shop.todayUniqueCustID.splice(ZERO, shop.todayUniqueCustID.length);
 }
 
 const cleaningSlots = () => {
@@ -240,7 +274,7 @@ const cleaningSlots = () => {
     $('.customer').css('background-image', '');
     $('.customer').empty();
     $('.customer').addClass('empty');
-
+    $('.customer').attr('custid', '');
 }
 
 const updateDisplay = () => {
@@ -265,7 +299,7 @@ const fetchCustomer = (unqID) => {
         url: 'https://www.superheroapi.com/api.php/10158656646217518/' + unqID
     }).then(
         (data) => {
-            todayCust.push(currCust);
+            shop.todayCust.push(currCust);
             currCust.custID = data.id;
             currCust.custName = data.name;
             currCust.custImage = data.image.url;
@@ -285,11 +319,11 @@ const fetchCustomer = (unqID) => {
 }
 
 const getUniqueSuperheroesID = () => {
-    let newSuperheroID = generateRandomSuperheroID();
-    while ((todayUniqueCustID.includes(newSuperheroID)) || (LIST_OF_BROKEN_HEROES.includes(newSuperheroID))) {
-        newSuperheroID = generateRandomSuperheroID();
+    let newSuperheroID = Math.ceil(Math.random() * NUM_OF_SUPERHEROES);
+    while ((shop.todayUniqueCustID.includes(newSuperheroID)) || (LIST_OF_BROKEN_HEROES.includes(newSuperheroID))) {
+        newSuperheroID = Math.ceil(Math.random() * NUM_OF_SUPERHEROES);;
     };
-    todayUniqueCustID.push(newSuperheroID);
+    shop.todayUniqueCustID.push(newSuperheroID);
     return newSuperheroID;
 }
 
