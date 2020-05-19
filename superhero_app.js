13ad921eab3e58f7eec83c1ee43b737f6c1df0c8
Linawaty <linawatylim@gmail.com>
2020-05-19 18:14:27 +0800

    Extracting superheroes

---
+++ superhero_app.js
@@ -0,0 +1,28 @@
+let broken = [51, 54, 74, 101, 113, 117, 131, 133, 134, 143, 164, 184, 205, 244, 283, 288, 290, 291, 292, 362, 447, 453, 511, 512, 552, 553, 593, 603, 629, 662, 682, 694, 698, 715, 721, 725];
+$(() => {
+    // for (let i = 0; i < 1 ; i++) {
+
+    //     $.ajax({
+    //         url: 'https://www.superheroapi.com/api.php/10158656646217518/' + i + '/image'
+    //     }).then(
+    //         (data) => {
+    //             $('#container')
+    //                     .append($('<div>')
+    //                         .addClass('superheroes'));
+    //                 $('.superheroes:last-of-type')
+    //                     .append($('<img>')
+    //                         .attr('src', data.url)
+    //                         .addClass('image'))
+    //                     .append($('<p>')
+    //                         .text('id: ' + data.id))
+    //                     .append($('<p>')
+    //                         .text('name: ' + data.name))
+    //                     ;
+
+    //         })
+    //         .catch(error => {
+    //             console.log('err: ', error);
+    //         });
+    // }
+
+});
