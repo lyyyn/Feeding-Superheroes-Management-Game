2c151f5601ef88137be74ba40b8e285e250de2db
Linawaty <linawatylim@gmail.com>
2020-05-20 10:32:31 +0800

    Fix the message for modal 2

--- project1_app.js
+++ project1_app.js
@@ -432,7 +432,10 @@ $modalButton.on('click', (Event) => {
 //main
 $(() => {
     //Day Cycle
-    setMessage(DAY_MESSAGE[shop.currDay], START_DAY);
+    setMessage(DAY_MESSAGE[shop.currDay], START_DAY,`Superheroes will teleport into our store soon. <br/>
+    Just drag the food they order to the Superheroes image to serve. <br/>
+    They will pay you when they leave, uhm.. <br/>
+    provided that you completed the order before store close. <br/>`);
     setTimeout(displayMessage, 1000);
 
 
